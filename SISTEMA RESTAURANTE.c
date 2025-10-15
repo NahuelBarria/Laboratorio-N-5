c1c8659c56dfd44f584ae8d2f08cff11ac5dbcbf
Nahu <125503938+NahuelBarria@users.noreply.github.com>
2025-10-15 15:24:37 -0300

    Add files via upload

---
+++ SISTEMA RESTAURANTE.c
@@ -0,0 +1,15 @@
+struct Plato {
+int id; // Identificador único del plato
+char nombre[MAX_TEXTO]; // Nombre del plato
+float precio; // Precio del plato
+int stock; // Cantidad disponible
+};
+
+struct Pedido {
+int id; // Identificador único del pedido
+char cliente[MAX_TEXTO]; // Nombre del cliente
+int idPlato; // ID del plato pedido
+int cantidad; // Cantidad solicitada
+float total; // Precio total del pedido
+int activo; // 1 = activo, 0 = completado
+};
